db327cb57dd278e4833738f38d519085c041cf63
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-12 18:55:15 +0800

    +adding new project for extensions

---
+++ Tarsier.Extensions/IntegerUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Tarsier.Extensions {
+    public static class IntegerUtils {
+        public static int ToSafeInteger(this object integerValue) {
+            int outInt = 0;
+            if(int.TryParse(integerValue.ToString(), out outInt)) {
+                return outInt;
+            }
+            return outInt;
+        }
+
+        public static int ToSafeInteger(this string integerValue) {
+            if(!String.IsNullOrWhiteSpace(integerValue)) {
+                int outInt = 0;
+                if(int.TryParse(integerValue.Trim(), out outInt)) {
+                    return outInt;
+                }
+            }
+            return 0;
+        }
+    }
+}
