a9ad825a087bc5b84d85bbdae5c55d53da0b3ffd
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-13 14:15:59 +0800

    -removing unused references

--- Password.Storage/PasswordForm.cs
+++ Password.Storage/PasswordForm.cs
@@ -3,19 +3,14 @@ using Password.Storage.Controllers;
 using Password.Storage.Forms;
 using Password.Storage.Properties;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using Tarsier.Regedit;
 
 namespace Password.Storage {
     public partial class PasswordForm : Form {
         private Credentials _pwds;
+        private Image lockImg = Resources._lock;
         private string _selectedId = string.Empty;
         private bool _showPanel = false;
         private bool _hidePassword = true;
@@ -220,8 +215,6 @@ namespace Password.Storage {
             if((e.ItemState & ListViewItemStates.Selected) == 0) {
                 e.DrawBackground();
             }
-
-            Image lockImg = Resources._lock;
             e.DrawDefault = false;
             //e.DrawBackground();
             e.Graphics.DrawImage(lockImg, e.SubItem.Bounds.Location);
