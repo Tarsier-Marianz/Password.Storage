818a8c98c4c05094b39d6aa4f2cfaa4d6191a183
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-13 09:22:23 +0800

    +add option form

---
+++ Password.Storage/Forms/OptionForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Password.Storage.Forms {
+    partial class OptionForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.chkEncryptContent = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkHidePassword = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chkEncryptContent
+            // 
+            this.chkEncryptContent.AutoSize = true;
+            this.chkEncryptContent.Location = new System.Drawing.Point(23, 26);
+            this.chkEncryptContent.Name = "chkEncryptContent";
+            this.chkEncryptContent.Size = new System.Drawing.Size(63, 17);
+            this.chkEncryptContent.TabIndex = 0;
+            this.chkEncryptContent.Text = "Encrypt";
+            this.chkEncryptContent.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(41, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Encrypt all data stored into database.";
+            // 
+            // chkHidePassword
+            // 
+            this.chkHidePassword.AutoSize = true;
+            this.chkHidePassword.Location = new System.Drawing.Point(23, 84);
+            this.chkHidePassword.Name = "chkHidePassword";
+            this.chkHidePassword.Size = new System.Drawing.Size(96, 17);
+            this.chkHidePassword.TabIndex = 2;
+            this.chkHidePassword.Text = "Hide Password";
+            this.chkHidePassword.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(41, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dont display password int the table.";
+            // 
+            // OptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chkHidePassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkEncryptContent);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "OptionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "OptionForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkEncryptContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkHidePassword;
+        private System.Windows.Forms.Label label2;
+    }
+}
