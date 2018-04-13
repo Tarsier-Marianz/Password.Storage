112850cf0c0f92f35d6ec1c34dcb38413faa863a
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-13 10:44:19 +0800

    +add constant variables for messages

--- Password.Storage/Constant/Messages.cs
+++ Password.Storage/Constant/Messages.cs
@@ -6,5 +6,7 @@ using System.Text;
 namespace Password.Storage.Constant {
     public class Messages {
         public const string EXIT = @"Are you sure you want to exit application?";
+        public const string SHOW = "Passwords will be displayed in the list.\nAre you sure you want to show password? ";
+        public const string CLEAR = "All credentials will be permanently deleted.\nAre you sure you want to delete all credentials? ";
     }
 }
