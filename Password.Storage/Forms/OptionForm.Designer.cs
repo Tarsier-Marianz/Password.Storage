d5fdae84e6456289e88fc20998682b016ec677e3
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-13 10:03:39 +0800

    *enhancing option section

--- Password.Storage/Forms/OptionForm.Designer.cs
+++ Password.Storage/Forms/OptionForm.Designer.cs
@@ -23,67 +23,125 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionForm));
             this.chkEncryptContent = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.chkHidePassword = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkShowDetails = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // chkEncryptContent
             // 
-            this.chkEncryptContent.AutoSize = true;
+            this.chkEncryptContent.Checked = true;
+            this.chkEncryptContent.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkEncryptContent.Enabled = false;
+            this.chkEncryptContent.ForeColor = System.Drawing.Color.Green;
             this.chkEncryptContent.Location = new System.Drawing.Point(23, 26);
             this.chkEncryptContent.Name = "chkEncryptContent";
-            this.chkEncryptContent.Size = new System.Drawing.Size(63, 17);
+            this.chkEncryptContent.Size = new System.Drawing.Size(292, 17);
             this.chkEncryptContent.TabIndex = 0;
+            this.chkEncryptContent.Tag = "Encrypt";
             this.chkEncryptContent.Text = "Encrypt";
             this.chkEncryptContent.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Gray;
             this.label1.Location = new System.Drawing.Point(41, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.Size = new System.Drawing.Size(274, 35);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Encrypt all data stored into database.";
+            this.label1.Text = "Encrypt all data stored into database. This will help to secure database content." +
+    "";
             // 
             // chkHidePassword
             // 
-            this.chkHidePassword.AutoSize = true;
+            this.chkHidePassword.ForeColor = System.Drawing.Color.Green;
             this.chkHidePassword.Location = new System.Drawing.Point(23, 84);
             this.chkHidePassword.Name = "chkHidePassword";
-            this.chkHidePassword.Size = new System.Drawing.Size(96, 17);
+            this.chkHidePassword.Size = new System.Drawing.Size(292, 17);
             this.chkHidePassword.TabIndex = 2;
+            this.chkHidePassword.Tag = "HidePassword";
             this.chkHidePassword.Text = "Hide Password";
             this.chkHidePassword.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Gray;
             this.label2.Location = new System.Drawing.Point(41, 104);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 13);
+            this.label2.Size = new System.Drawing.Size(274, 32);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Dont display password int the table.";
+            this.label2.Text = "Do not display password in the credential table. Instead asterisk (*) will be dis" +
+    "played.";
+            // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(41, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Display panel containing details of selected credential located in right side of " +
+    "the window.";
+            // 
+            // chkShowDetails
+            // 
+            this.chkShowDetails.ForeColor = System.Drawing.Color.Green;
+            this.chkShowDetails.Location = new System.Drawing.Point(23, 143);
+            this.chkShowDetails.Name = "chkShowDetails";
+            this.chkShowDetails.Size = new System.Drawing.Size(292, 17);
+            this.chkShowDetails.TabIndex = 4;
+            this.chkShowDetails.Tag = "ShowPanel";
+            this.chkShowDetails.Text = "Show Detail Panel";
+            this.chkShowDetails.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(255, 224);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 26);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(174, 224);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 26);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Apply";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // OptionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 262);
+            this.ClientSize = new System.Drawing.Size(342, 265);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkShowDetails);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.chkHidePassword);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.chkEncryptContent);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "OptionForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "OptionForm";
+            this.Text = "Options";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -93,5 +151,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox chkHidePassword;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkShowDetails;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
     }
 }
---
+++ Password.Storage/Forms/OptionForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tarsier.Regedit;
+
+namespace Password.Storage.Forms {
+    public partial class OptionForm : Form {
+        public OptionForm() {
+            InitializeComponent();
+            InitOptions();
+        }
+
+        private void InitOptions() {
+            chkEncryptContent.Checked = RegConfig.Get<bool>("Encrypt");
+            chkHidePassword.Checked = RegConfig.Get<bool>("HidePassword");
+            chkShowDetails.Checked = RegConfig.Get<bool>("ShowPanel");
+        }
+        private void AppyOptions() {
+            RegConfig.Set<bool>("Encrypt", chkEncryptContent.Checked);
+            RegConfig.Set<bool>("HidePassword", chkHidePassword.Checked);
+            RegConfig.Set<bool>("ShowPanel", chkShowDetails.Checked);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            AppyOptions();
+            Close();
+        }
+    }
+}
